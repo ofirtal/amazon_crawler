0ae3d11c0707cad2f2967736dd903d0b56dc3b2f
ofir tal <ofirtal2511@gmail.com>
2019-09-10 14:00:29 +0300

    changes in generate_txt_file structure and some functions /varia

--- amazon_products.py
+++ amazon_products.py
@@ -1,3 +1,11 @@
+# A scraper designed to search amazon for search terms (from a .csv file) and scrape
+# the ASIN numbers of the products that appear in the search results.
+# The code then compares the ASIN numbers to a target list and categorizes the search terms
+# into three categories: Good(returns more than 3 target ASIN numbers),
+# Needs further check (1-3) and Bad (0).
+# results are returned in the form of a .txt file.
+
+
 from multiprocessing import Pool
 from bs4 import BeautifulSoup
 import requests
@@ -20,17 +28,19 @@ def connect_to_amazon(URL):
         target_web_page.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print("Error: " + str(e))
+        exit()
 
     # if target_web_page.status_code == 200:
     soup = BeautifulSoup(target_web_page.content, 'html.parser')
     data_asin_list = []
     results = soup.find_all('div', {'class': "s-result-list s-search-results sg-row"})
     count = 0
+    returned_products_from_page = 19
     for link in results:
         for div in link:
             if div is None or div == "\n":
                 continue
-            if count > 19:
+            if count > returned_products_from_page:
                 break
             elif div.get('data-asin') is not None and div.get('data-asin') != "":
                 data_asin_list.append([div.get('data-asin'), div.get('data-index')])
@@ -40,37 +50,41 @@ def connect_to_amazon(URL):
 
 
 # in: data_asin_list, out: number of target data asin in the data asin product list
-def compare(list):
+def num_of_occurances_in_list(data_asin_list):
     count_seen = 0
-    for item in list:
+    for item in data_asin_list:
         if item[0] in asin_db:
             count_seen += 1
     return count_seen
 
 
 # pairing word with count
-def multi_grt(word):
+def phrase_and_target_asin_count_dict(word):
     data_asin_dict = connect_to_amazon(search_db(word))
-    count = compare(data_asin_dict)
+    count = num_of_occurances_in_list(data_asin_dict)
     return {word: count}
+
 
 # generates a txt file with the full data
 def generate_txt_file(good_words_list, check_words_list, bad_words_list):
     timestr = time.strftime("%d%m%Y-%H%M%S")
     with open("amazon_{}.txt".format(timestr), "w") as f:
-        for list_item in (good_words_list, check_words_list, bad_words_list):
-            if list_item == good_words_list:
-                f.write("Good words: ")
-            elif list_item == check_words_list:
-                f.write("Need to check: ")
-            elif list_item == bad_words_list:
-                f.write("Bad words: ")
-            f.write(str(len(list_item)))
-            f.write("\n")
-            for i in list_item:
-                f.write(i)
-                f.write("\n")
-            f.write("\n\n\n")
+        f.write("Good words: ")
+        txt_file_body(good_words_list, f)
+        f.write("\n")
+        f.write("Need to check: ")
+        txt_file_body(check_words_list, f)
+        f.write("\n")
+        f.write("Bad words: ")
+        txt_file_body(bad_words_list, f)
+
+
+def txt_file_body(list_item, f):
+    f.write(str(len(list_item)))
+    f.write("\n")
+    for i in list_item:
+        f.write(i)
+        f.write("\n")
 
 
 def main():
@@ -82,23 +96,25 @@ def main():
     # search_words : list of search terms from csv
     search_words = search_word()
 
-    p = Pool()
-    count_list = p.map(multi_grt, search_words)
+    pool_assign = Pool()
+    count_list = pool_assign.map(phrase_and_target_asin_count_dict, search_words)
+    good_word_min = 3
     for item in count_list:
         for i in item:
             if item[i] == 0:
                 bad_words.append(i)
-            elif item[i] >= 3:
+            elif item[i] >= good_word_min:
                 good_word.append(i)
             else:
                 need_to_check_words.append(i)
-    p.close()
-    p.join()
+    pool_assign.close()
+    pool_assign.join()
 
     generate_txt_file(good_word, need_to_check_words, bad_words)
     print("good words  :", len(good_word), "\n", good_word, "\n")
     print("need to check words  :", len(need_to_check_words), "\n", need_to_check_words,"\n")
     print("bad words  :", len(bad_words), "\n", bad_words, "\n")
+
 
 if __name__ == '__main__':
     main()
